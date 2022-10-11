4b2b11cc72cfbcf9588b27dc48b9daec57f60d48
tokarev.s <tokarev.s@senatnn.ru>
2022-10-11 16:35:49 +0300

    Modify script

--- CatalogBuy.py
+++ CatalogBuy.py
@@ -1,16 +1,18 @@
-# Данный скрипт проверяет добавление нескольких товарных изделий в корзину, ввод пользовательских данных
+# Данный скрипт проверяет добавление нескольких товарных изделий в корзину через каталог, ввод пользовательских данных
 # и оформление заказа.
 
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 catalog = "https://senatnn.ru/catalog/ukrasheniya/"
 ordering = 'https://senatnn.ru/cart/ordering/'
 jewelry_amount = 3
 
-#функция для добавления в корзину N позиций
+#функция для добавления в корзину N позиций через страницу каталога
 def buy_jewelry(n:int):
     for i in (range(1, n+1)):
         driver.find_element(By.CSS_SELECTOR, f".senat-cat-item:nth-child({i}) .btn-can-buy").click()
@@ -23,15 +25,15 @@ try:
     driver.get(catalog)
 
 # Jivosite должен успеть подгрузиться чтобы мы его отключили, поэтому ставим таймер
-    time.sleep(5) 
-    driver.execute_script("document.getElementsByClassName('globalClass_dc4a')[0].remove()",)
+    WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.TAG_NAME, "jdiv")))
+    driver.execute_script("document.getElementsByTagName('jdiv')[0].remove()")
     
 # Добавляем n изделий в корзину    
     buy_jewelry(jewelry_amount)
 
 # Переходим на страницу оформления заказа
     driver.get(ordering)
-    time.sleep(3)
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".r3x1 input")))
     
 # Создаем массив с локаторами полей ввода
     input_fields = driver.find_elements(By.CSS_SELECTOR, ".r3x1 input")  
